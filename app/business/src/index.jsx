05a6dfa3dcc7fc48886c1f8059af7206491c085d
tienlx97 <tienlx97@gmail.com>
2024-09-19 22:45:25 +0700

    chore: 1

--- app/business/src/index.jsx
+++ app/business/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Utils } from '@meta-core/utils/index';
+import { env } from '@meta-core/utils/index';
 
 const rootElement = document.getElementById('root');
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(<div>{Utils}</div>);
+  root.render(<div>{env.start}</div>);
 }
--- packages/plugins/eslint/index.js
+++ packages/plugins/eslint/index.js
@@ -4,7 +4,7 @@ const OFF = 0;
 const ERROR = 2;
 
 module.exports = {
-  // Prettier must be last so it can override other configs
+  // Prettier must be last so it can override other configs (https://github.com/prettier/eslint-config-prettier#installation)
   extends: ['alloy', 'alloy/react'],
 
   globals: {
@@ -20,25 +20,45 @@ module.exports = {
 
   overrides: [
     {
-      // node scripts should be console logging so don't lint against that
-      files: ['scripts/**/*.js'],
+      env: {
+        browser: true,
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+      },
+      plugins: ['react', '@typescript-eslint'],
       rules: {
-        'no-console': OFF,
+        '@typescript-eslint/ban-ts-comment': OFF,
+        '@typescript-eslint/no-this-alias': OFF,
+        '@typescript-eslint/no-unused-vars': [ERROR, { args: 'none' }],
       },
     },
     {
-      files: ['*.js', '*.jsx'],
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
       rules: {
         'simple-import-sort/imports': [
           'error',
           {
             groups: [
-              ['^react', '^@?\\w'], // Packages `react` related packages come first.
-              ['^(@|components)(/.*|$)'], // Internal packages.
-              ['^\\u0000'], // Side effect imports.
-              ['^\\.\\.(?!/?$)', '^\\.\\./?$'], // Parent imports.
-              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'], // Other relative imports.
-              ['^.+\\.?(css)$'], // Style imports.
+              // Packages `react` related packages come first.
+              ['^react', '^@?\\w'],
+              // Internal packages.
+              ['^(@|components)(/.*|$)'],
+              // Side effect imports.
+              ['^\\u0000'],
+              // Parent imports. Put `..` last.
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Other relative imports. Put same-folder imports and `.` last.
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports.
+              ['^.+\\.?(css)$'],
             ],
           },
         ],
@@ -62,29 +82,68 @@ module.exports = {
     'simple-import-sort',
     'sort-keys-fix',
     'import',
-    // 'header',
     'no-function-declare-after-return',
     'no-only-tests',
     '@stylexjs',
   ],
 
+  // Stop ESLint from looking for a configuration file in parent folders
   root: true,
-
+  // We're stricter than the default config, mostly. We'll override a few rules
+  // and then enable some React specific ones.
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    // increase the severity of rules so they are auto-fixable
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
+
+    // *
     '@stylexjs/valid-styles': ['error'],
+
     eqeqeq: [ERROR, 'allow-null'],
-    // 'header/header': [2, 'scripts/www/headerTemplate.js'],
+
+    // *
+    // (This helps configure simple-import-sort) Make sure all imports are at the top of the file
     'import/first': ERROR,
+
+    // *
+    // (This helps configure simple-import-sort) Make sure there's a newline after the imports
     'import/newline-after-import': ERROR,
+
+    // *
+    // (This helps configure simple-import-sort) Merge imports of the same file
     'import/no-duplicates': ERROR,
+
+    indent: OFF,
+
     'jsx-quotes': [ERROR, 'prefer-double'],
+
+    // Enforced by Prettier
+    // TODO: Prettier doesn't handle long strings or long comments. Not a big
+    // deal. But I turned it off because loading the plugin causes some obscure
+    // syntax error and it didn't seem worth investigating.
+    'max-len': OFF,
+
+    // 'max-len': ["error", { "code": 80 }],
+
+    // 'no-multi-spaces': ERROR,
+
+    'no-unused-expressions': OFF, // ERROR
+
     'no-unused-vars': [ERROR, { args: 'none' }],
+
+    // We apply these settings to files that should run on Node.
+    // They can't use JSX or ES6 modules, and must be in strict mode.
+    // They can, however, use other ES6 features.
+    // (Note these rules are overridden later for source files.)
     'no-var': ERROR,
+
     'no-restricted-globals': [ERROR].concat(restrictedGlobals),
+
     'no-param-reassign': OFF,
     'no-void': OFF,
+
     'react/no-children-prop': OFF,
   },
 };
